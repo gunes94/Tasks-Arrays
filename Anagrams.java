c15de8e0cf7f1b673fc4c5a8d912acf3b2f75fef
Gyunesh Mehmed <30699247+gunes94@users.noreply.github.com>
2024-01-12 17:46:53 +0200

    Create Anagrams.java

---
+++ Anagrams.java
@@ -0,0 +1,31 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+/*Направeте програма, която прочита две думи от конзолата и отпечатва true, ако те са анаграми
+и false, ако не са. Абстрахирайте се от големината на буквите.*/
+
+public class Anagrams {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        char[] firstWord = scanner.next().toLowerCase().toCharArray();
+        char[] secondWord = scanner.next().toLowerCase().toCharArray();
+
+        boolean isAnagram = true;
+
+        if (firstWord.length != secondWord.length) {
+            isAnagram = false;
+        }
+
+        Arrays.sort(firstWord);
+        Arrays.sort(secondWord);
+
+        for (int i = 0; i < firstWord.length; i++) {
+            if (firstWord[i] != secondWord[i]) {
+                isAnagram = false;
+            }
+        }
+
+        System.out.println(isAnagram);
+    }
+}
